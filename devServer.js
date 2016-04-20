49069e146d1000e5746b702eb1b8c75e9815d102
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-04-20 10:19:17 +0700

    edit stm

--- devServer.js
+++ devServer.js
@@ -18,7 +18,7 @@ app.use('/public', express.static('public'));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-let port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
